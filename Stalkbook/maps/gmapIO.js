827c208b2d00a855bc196090cb3b0236f46aa13c
thompsbenn <thompsbenn@daa53a37-a638-0410-928d-cbb12661f8bf>
2007-08-20 22:18:55 +0000

    Added comments, also now has optional zoom levels defined for setPosition functions.

--- Stalkbook/maps/gmapIO.js
+++ Stalkbook/maps/gmapIO.js
@@ -20,12 +20,13 @@ var StalkBook = {
   locality: "christchurch, new zealand",
   geocoder: new GClientGeocoder(),
   map: undefined,
-  //currentLocation: new GPoint(),
   
+  /*
+  Loads up the map requires map element. Define interfaces here.
+  */
   load: function() {
     if (GBrowserIsCompatible()) {
       StalkBook.map=new GMap2(document.getElementById("map"));
-      //map.setCenter(new GLatLng(-40.979898,170.671875), 16);
       StalkBook.geocoder.getLatLng(StalkBook.locality,function(point){
 		StalkBook.map.setCenter(point, 12);
       });
@@ -44,33 +45,46 @@ var StalkBook = {
       })
     }
   },
-  displayInfo: function(marker,str) {
-	marker.openInfoWindowHtml(str);
+
+
+/*
+Add marker at a given point defined by latitude(lat) and longitude(lng) also takes an html 
+formated string(str) that will be displayed when the marker is clicked.
+*/
+  addMarker: function(lat, lng, str){
+    var point= new GLatLng(lat, lng);
+    var marker = new GMarker(point);
+	GEvent.addListener(marker, "click", function(){
+		marker.openInfoWindowHtml(str);
+	});
+  	StalkBook.map.addOverlay(marker);
   },
 
-  createMarker: function(point,str) {
-	var marker = new GMarker(point);
-	GEvent.addListener(marker, "click", function(){
-		StalkBook.displayInfo(marker,str);
-	});
-	return marker;
+/*
+Centers position to a given point, defined by latitude(lat) and longitude(lng). 
+This has an optional zoom parameter. This can be between 1-17 possibly more 
+for areas with more detail.
+*/  
+  setPositionXY: function(lat, lng, zoom){
+  if(!zoom)zoom=StalkBook.map.getZoom();
+  	StalkBook.map.setCenter(new GLatLng(lat,lng,true), zoom);
   },
   
-  addMarker: function(x, y, str){
-  	StalkBook.map.addOverlay(StalkBook.createMarker(new GLatLng(x,y),str));
-  },
-  
-  setPositionXY: function(x, y){
-  	StalkBook.map.setCenter(new GLatLng(x,y,true));
-  	//StalkBook.map.setCenter(new GLatLng(37.4419, -122.1419), 13);
-  },
-  
-  setPositionString: function(location){
+ /*
+ Centers position to a place defined by a string. This has an optional zoom 
+ parameter. This can be between 1-17 possibly more for areas with more detail.
+ */
+  setPositionString: function(location, zoom){
+    if(!zoom)zoom=StalkBook.map.getZoom();
   	StalkBook.geocoder.getLatLng(location,function(point){
-		StalkBook.map.setCenter(point);
+		StalkBook.map.setCenter(point,zoom);
     });
   },
   
+  
+ /*
+ Converts a GLatLng to a point object.
+ */ 
   gpointtopoint: function(latlngpoint) {
   	var point = {
   	  lat: latlngpoint.lat(),
@@ -78,5 +92,7 @@ var StalkBook = {
     };
     return point;
   }
+  
+  
 };
 //]]>
--- Stalkbook/maps/gmapTest.js
+++ Stalkbook/maps/gmapTest.js
@@ -8,9 +8,10 @@ test: function() {
 	
 
 },
-testxy: function() {StalkBook.setPositionXY(gmapTest.testX,gmapTest.testY);},
-teststring: function() {StalkBook.setPositionString(gmapTest.testString);},
-testaddtag: function() {StalkBook.addMarker(gmapTest.testX,gmapTest.testY,"<img src=\"http://tbn0.google.com/images?q=tbn:3ID-YoLsX678oM:http://mondomonkey.com/MondoMonkeyWhiteB.jpg\"\>");}
+testxy: function() {StalkBook.setPositionXY(gmapTest.testX,gmapTest.testY,5);},
+teststring: function() {StalkBook.setPositionString(gmapTest.testString,10);},
+testaddtag: function() {StalkBook.addMarker(gmapTest.testX,gmapTest.testY,"<img src=\"http://tbn0.google.com/images?q=tbn:3ID-YoLsX678oM:http://mondomonkey.com/MondoMonkeyWhiteB.jpg\"\>");},
+teststringWithoutZoom: function() {StalkBook.setPositionString(gmapTest.testString);}
 
 
 
