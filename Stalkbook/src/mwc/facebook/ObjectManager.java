5eafe93f39d5d52710fbbabcaf8e5f6b0fcfc288
stephen <stephen@daa53a37-a638-0410-928d-cbb12661f8bf>
2007-08-21 02:02:27 +0000

    ObjectManager for managing persistent objects

---
+++ Stalkbook/src/mwc/facebook/ObjectManager.java
@@ -0,0 +1,48 @@
+package mwc.facebook;
+
+import java.sql.SQLException;
+
+import mwc.facebook.data.DataStore;
+import mwc.facebook.data.MockDataStore;
+import mwc.facebook.data.PostgresDataStore;
+
+/**
+ * ObjectManager uses the singleton pattern to provide access to
+ * static objects for the lifetime of the server.
+ * 
+ * At the moment the only object managed is the DataStore.
+ * 
+ * @author stephen
+ *
+ */
+public class ObjectManager {
+	
+	public ObjectManager() {
+		try {
+			store = new PostgresDataStore("localhost", "stalkbook", "facebook", "");
+		}
+		catch (SQLException ex) {
+			System.err.println("Error opening SQL Database");
+			ex.printStackTrace();
+			
+			store = new MockDataStore();
+		}
+	}
+	
+	public DataStore store() {
+		return store;
+	}
+	
+	private DataStore store;
+	
+	static {
+		instance = new ObjectManager();
+	}
+	
+	public static ObjectManager instance() {
+		return instance;
+	}
+	
+	private static ObjectManager instance;
+
+}
